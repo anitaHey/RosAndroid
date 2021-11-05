9e21b542b323789ac0669a17fdc8ad7b7f506556
anitaHey <jiaa.anita@gmail.com>
2021-11-05 11:20:19 +0800

    Init

---
+++ app/src/main/java/com/anita/rosandroid/MainActivity.java
@@ -0,0 +1,6 @@
+package com.anita.rosandroid;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
---
+++ app/src/main/java/com/anita/rosandroid/RCApplication.java
@@ -0,0 +1,28 @@
+package com.anita.rosandroid;
+
+import android.app.Application;
+import com.jilk.ros.rosbridge.ROSBridgeClient;
+
+public class RCApplication extends Application {
+    ROSBridgeClient client;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        if(client != null)
+            client.disconnect();
+        super.onTerminate();
+    }
+
+    public ROSBridgeClient getRosClient() {
+        return client;
+    }
+
+    public void setRosClient(ROSBridgeClient client) {
+        this.client = client;
+    }
+}
